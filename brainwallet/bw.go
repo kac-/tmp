077b9d73ec1aa68424faa7071aa1a6bc0a3ffff6
kac- <6b6163@gmail.com>
2014-04-04 09:13:32 +0200

    brainwallet

---
+++ brainwallet/bw.go
@@ -0,0 +1,35 @@
+package main
+
+import(
+    "os"
+    "bufio"
+    "strings"
+    "fmt"
+    "github.com/conformal/btcec"
+    "github.com/conformal/btcutil"
+    "github.com/conformal/btcwire"
+    "github.com/conformal/fastsha256"
+)
+
+
+func main() {
+    lines := bufio.NewReader(os.Stdin)
+    hasher := fastsha256.New()
+    for {
+        line, _, err := lines.ReadLine()
+        if err != nil { break }
+        
+        var str = strings.Trim(string(line), " ")
+        if ( len(str) == 0 ){ continue }
+    
+        hasher.Reset()
+        hasher.Write([]byte(str))
+        sum := hasher.Sum( nil )
+
+        _, pub := btcec.PrivKeyFromBytes(btcec.S256(), sum)
+        var apk, _ = btcutil.NewAddressPubKey(pub.SerializeUncompressed(), btcwire.MainNet)
+        fmt.Println(apk.EncodeAddress())
+        apk, _ = btcutil.NewAddressPubKey(pub.SerializeCompressed(), btcwire.MainNet)
+        fmt.Println(apk.EncodeAddress())
+    }
+}
